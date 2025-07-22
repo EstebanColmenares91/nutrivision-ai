4cd2af7e484220faeacb428c3d29babab89d4e07
Esteban Colmenares <esteban.colmenares@urbe.edu>
2025-07-22 10:47:45 -0400

    Add stautsBar

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,26 +1,30 @@
 import Ionicons from "@expo/vector-icons/Ionicons";
 import { router, Stack } from "expo-router";
+import { StatusBar } from "expo-status-bar";
 import { TouchableOpacity } from "react-native";
 
 export default function RootLayout() {
   return (
-    <Stack>
-      <Stack.Screen
-        name="index"
-        options={{ headerShown: false }}
-      ></Stack.Screen>
-      <Stack.Screen
-        name="analyze"
-        options={{
-          presentation: "modal",
-          title: "Nutrition Analysis",
-          headerLeft: () => (
-            <TouchableOpacity onPress={() => router.back()}>
-              <Ionicons name="close" size={24} color="black" />
-            </TouchableOpacity>
-          ),
-        }}
-      ></Stack.Screen>
-    </Stack>
+    <>
+      <StatusBar style="dark" />
+      <Stack>
+        <Stack.Screen
+          name="index"
+          options={{ headerShown: false }}
+        ></Stack.Screen>
+        <Stack.Screen
+          name="analyze"
+          options={{
+            presentation: "modal",
+            title: "Nutrition Analysis",
+            headerLeft: () => (
+              <TouchableOpacity onPress={() => router.back()}>
+                <Ionicons name="close" size={24} color="black" />
+              </TouchableOpacity>
+            ),
+          }}
+        ></Stack.Screen>
+      </Stack>
+    </>
   );
 }
